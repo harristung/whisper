18b49cde94547e0030b962c2f161a5a7e71b6d6a
harristung <harristung@gmail.com>
2025-01-06 19:14:11 +0800

    Update tokenizer.py
    Bug with the self.num_languages, which is missing the last index "yue", added 1 to include the last index to aviod user getting error when language is set to "yue"

--- whisper/tokenizer.py
+++ whisper/tokenizer.py
@@ -148,7 +148,7 @@ class Tokenizer:
         translate: int = self.special_tokens["<|translate|>"]
         transcribe: int = self.special_tokens["<|transcribe|>"]
 
-        langs = tuple(LANGUAGES.keys())[: self.num_languages]
+        langs = tuple(LANGUAGES.keys())[: self.num_languages + 1]
         sot_sequence = [sot]
         if self.language is not None:
             sot_sequence.append(sot + 1 + langs.index(self.language))
